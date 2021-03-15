3c2b2705f0e7321eb978a12e9a17bb9ec2233138
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-03-15 11:07:25 -0700

    Update entrypoint

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
-if __name__ == '__main__':
-    print('Hello world')
+from argparse import ArgumentParser
+from importlib import import_module
+
+
+def _load_module(name):
+    mod_name = f"benchmark_{name}"
+    return import_module(f"benchmarks.{mod_name}")
+
+
+def _get_benchmark(name, mod):
+    cls_name = f"Benchmark{name.upper()}"
+    ctor = getattr(mod, cls_name)
+
+    return ctor()
+
+
+parser = ArgumentParser(description="Run arch benchmarks")
+parser.add_argument("name", help="Name of benchmark to run", choices=["s3"])
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    mod = _load_module(args.name)
+    benchmark = _get_benchmark(args.name, mod)
+
+    benchmark.run()
