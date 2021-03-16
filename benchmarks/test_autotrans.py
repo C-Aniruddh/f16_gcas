6dca2564c55c1f11b273a446099b1dcd54e3eebe
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-03-16 16:37:56 -0700

    Create autotrans test case

---
+++ benchmarks/test_autotrans.py
@@ -0,0 +1,22 @@
+from os import path
+from unittest import TestCase
+
+from numpy import array
+from numpy.testing import assert_equal
+from pandas import read_csv
+
+from ._autotrans import simulate_autotrans
+
+
+class AutotransTestCase(TestCase):
+    def test_correct_output(self):
+        test_dir = path.dirname(__file__)
+        inputs = read_csv(path.join(test_dir, "test_autotrans_inputs.csv"))
+        outputs = read_csv(path.join(test_dir, "test_autotrans_outputs.csv"))
+
+        simT = 30
+        T = inputs["t"].tonumpy()
+        U = inputs[["u1", "u2"]].tonumpy()
+        trajectories, _ = simulate_autotrans(simT, T, U)
+
+        assert_equal(outputs.tonumpy(), trajectories)
