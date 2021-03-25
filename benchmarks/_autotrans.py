3fcaea2fd1ae08aa91da9a81a711228857cba2c7
Aniruddh Chandratre <aniruddh.chandratre@gmail.com>
2021-03-24 17:38:31 -0700

    fix: update autotrans for matlab engine
    Signed-off-by: Aniruddh Chandratre <aniruddh.chandratre@gmail.com>

--- benchmarks/_autotrans.py
+++ benchmarks/_autotrans.py
@@ -5,13 +5,16 @@ import numpy as np
 def sim_autotrans(simT, T, u):
     eng = matlab.engine.connect_matlab()
 
-    eng.workspace["u"] = u
-    eng.workspace["T"] = T
+    eng.workspace["u"] = u.tolist()
+    eng.workspace["T"] = T.tolist()
+
+    print("u", u)
+    print("T", T)
 
     simopt = eng.simget("Autotrans_shift")
     simopt = eng.simset(simopt, "SaveFormat", "Array")
     timestamps, _, data = eng.sim(
-        "Autotrans_shift", [0, simT], simopt, hstack((T, u)), nargout=3
+        "Autotrans_shift", [0, float(simT)], simopt, np.row_stack((T, u)).astype(float).tolist(), nargout=3
     )
     np_data = np.array(data)
 
