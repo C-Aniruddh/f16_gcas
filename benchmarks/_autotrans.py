903e94ba96acfaab3c4d052ff7612c1ff2f2c0ed
Quinn Thibeault <qthibeau@asu.edu>
2021-03-25 09:53:17 -0700

    Start new matlab instance for every test because reusing the process was failing

--- benchmarks/_autotrans.py
+++ benchmarks/_autotrans.py
@@ -7,12 +7,13 @@ def sim_autotrans(simT, T, u):
     simT = matlab.double([0, float(simT)])
     simInp = matlab.double(np.row_stack((T, u)).T.astype(float).tolist())
 
-    eng = matlab.engine.connect_matlab()
+    eng = matlab.engine.start_matlab()
     simOpt = eng.simget("Autotrans_shift")
     simOpt = eng.simset(simOpt, "SaveFormat", "Array")
     timestamps, _, data = eng.sim("Autotrans_shift", simT, simOpt, simInp, nargout=3)
+    eng.quit()
 
-    np_timestamps = np.array(timestamps, dtype=np.float64).flatten()
+    np_timestamps = np.array(timestamps, dtype=np.float32).flatten()
     np_data = np.array(data, dtype=np.float64).T
 
     return np_data[0:2], np_timestamps
