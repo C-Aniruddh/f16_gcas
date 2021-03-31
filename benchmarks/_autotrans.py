5af88f6ecac76748fdef19723ea1bfc0dbea437e
Quinn Thibeault <qthibeau@asu.edu>
2021-03-30 18:16:20 -0700

    Keep engine handles instead of reconnecting After testing, this is much faster

--- benchmarks/_autotrans.py
+++ benchmarks/_autotrans.py
@@ -2,17 +2,16 @@ import matlab
 import matlab.engine
 import numpy as np
 
+engs = {}
+
 
 def sim_autotrans(simT, T, u, tag = "default"):
-    matlab_sessions = matlab.engine.find_matlab()
-    eng_name = f"benchmark_{tag}"
+    global engs
 
-    if eng_name not in matlab_sessions:
-        eng = matlab.engine.start_matlab()
-        eng.shareEngine(eng_name)
-    else:
-        eng = matlab.engine.connect_matlab(eng_name)
+    if tag not in engs:
+        engs[tag] = matlab.engine.start_matlab()
 
+    eng = engs[tag]
     simT = matlab.double([0, float(simT)])
     simInp = matlab.double(np.row_stack((T, u)).T.tolist())
     simOpt = eng.simget("Autotrans_shift")
