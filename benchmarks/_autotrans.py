72ce6516ac7828950e8cc099eae154e6c121ece7
Quinn Thibeault <qthibeau@asu.edu>
2021-03-30 17:12:14 -0700

    Try to reuse matlab instances to speed up simulation time

--- benchmarks/_autotrans.py
+++ benchmarks/_autotrans.py
@@ -3,15 +3,21 @@ import matlab.engine
 import numpy as np
 
 
-def sim_autotrans(simT, T, u):
-    simT = matlab.double([0, float(simT)])
-    simInp = matlab.double(np.row_stack((T, u)).T.astype(float).tolist())
+def sim_autotrans(simT, T, u, tag = "default"):
+    matlab_sessions = matlab.engine.find_matlab()
+    eng_name = f"benchmark_{tag}"
 
-    eng = matlab.engine.start_matlab()
+    if eng_name not in matlab_sessions:
+        eng = matlab.engine.start_matlab()
+        eng.shareEngine(eng_name)
+    else:
+        eng = matlab.engine.connect_matlab(eng_name)
+
+    simT = matlab.double([0, float(simT)])
+    simInp = matlab.double(np.row_stack((T, u)).T.tolist())
     simOpt = eng.simget("Autotrans_shift")
     simOpt = eng.simset(simOpt, "SaveFormat", "Array")
     timestamps, _, data = eng.sim("Autotrans_shift", simT, simOpt, simInp, nargout=3)
-    eng.quit()
 
     np_timestamps = np.array(timestamps, dtype=np.float32).flatten()
     np_data = np.array(data, dtype=np.float64).T
