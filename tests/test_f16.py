16b93f47ec84edc25a98543620a81730413f5129
Quinn Thibeault <qthibeau@asu.edu>
2021-04-02 09:05:57 -0700

    Add f16 test case

---
+++ tests/test_f16.py
@@ -0,0 +1,23 @@
+from math import pi
+
+from aerobench.run_f16_sim import run_f16_sim
+from aerobench.examples.gcas.gcas_autopilot import GcasAutopilot
+from benchmarks.models.f16 import f16_blackbox
+from numpy import array, deg2rad, float32, float64
+
+
+def test_f16_blackbox():
+    X = [
+            9,  # power
+            deg2rad(2.1215),  # alpha
+            0,  # beta
+            1000,  # alt
+            540,  # vel
+            -pi / 8,  # phi
+            (-pi / 2) * 0.3,  # theta
+            0  # psi
+    ]
+    T = [3.51]
+
+    f16_blackbox._func(X, T, [])
+
