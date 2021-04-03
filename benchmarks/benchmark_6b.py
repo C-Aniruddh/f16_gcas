2fd1536e415a6a1cd5f55960eafd78bc1c454a44
Quinn Thibeault <qthibeau@asu.edu>
2021-04-02 21:13:47 -0700

    Update benchmark 6b

--- benchmarks/benchmark_6b.py
+++ benchmarks/benchmark_6b.py
@@ -8,13 +8,14 @@ from ._autotrans import simulate_autotrans
 from ._benchmark import Benchmark
 
 
-def _6a_blackbox(_, T, u):
-    return sim_autotrans(_, max(T), T, u)
+@Blackbox
+def 6b_blackbox(_, T, u):
+    return sim_autotrans(max(T), T, u, "6b")
 
 
-class Benchmark6b(Benchmark):
+class Benchmark6B(Benchmark):
     def __init__(self):
-        self.phi = "([]_[0, 30] (rpm3000) ->[]_[0, 8] (speed50))"
+        self.phi = "([]_[0, 30] (rpm3000) -> []_[0, 8] (speed50))"
         self.preds = {
             "rpm3000": Predicate("rpm3000", array([0, 1]), array([3000])),
             "speed50": Predicate("speed50", array([1, 0]), array([50])),
@@ -23,13 +24,37 @@ class Benchmark6b(Benchmark):
             runs=50,
             iterations=100,
             seed=131013014,
+            interval=(0, 30),
             static_parameters=[],
             signals=[
                 SignalOptions((0, 100), control_points=7),
                 SignalOptions((0, 350), control_points=3),
             ],
         )
-        self.model = Blackbox(_6a_blackbox)
+        self.optimizer_options = PartitioningOptions(
+            subregion_file = "/tmp/subregions_benchmark6b.csv",
+            region_dimension = 10,
+            num_partition = 2,
+            miscoverage_level = 0.05,
+            num_sampling = 20,
+            level = [0.5, 0.75, 0.9, 0.95],
+            min_volume = 0.001,
+            max_budget = 15_000,
+            fal_num = 50_000,
+            n_model = 20,
+            n_bo = 10,
+            n_b = 100,
+            sample_method = SamplingMethod.BAYESIAN,
+            part_num = 1
+        )
+
 
     def run(self):
-        return staliro(self.phi, self.preds, self.model, self.options, partitioning)
+        return staliro(
+                self.phi,
+                self.preds,
+                6b_blackbox,
+                self.options,
+                partitioning,
+                self.optimizer_options
+        )
