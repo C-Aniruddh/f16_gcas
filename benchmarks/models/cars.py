6f87f64de8d28545e48b95b1ba011ade10ac707c
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-04-03 19:09:37 -0700

    Implement cars blackbox function

--- benchmarks/models/cars.py
+++ benchmarks/models/cars.py
@@ -1,6 +1,21 @@
+from matlab import double as mdouble
+from matlab.engine import start_matlab
+from numpy import array, float32, float64, row_stack
 from pystaliro.models import Blackbox
+
+engine = start_matlab()
+MODEL_NAME = "cars"
 
 
 @Blackbox
 def cars_blackbox(X, T, U):
-    pass
+    sim_opts = engine.simget(MODEL_NAME)
+    sim_opts = engine.simset(sim_opts, "SaveFormat", "Array")
+    sim_u = mdouble(row_stack(T, U).T.tolist())
+    sim_t = mdouble([0, max(T)])
+
+    timestamps, _, data = engine.sim(MODEL_NAME, sim_t, sim_opts, sim_u, nargout=2)
+    np_timestamps = array(timestamps, dtype=float32).flatten()
+    np_data = array(data, dtype=float64)
+
+    return np_data, np_timestamps
