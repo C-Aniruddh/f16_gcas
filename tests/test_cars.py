452b5f7581d6c74f9fa8168575aa6479a68580d2
Quinn Thibeault <qthibeau@asu.edu>
2021-04-05 17:52:30 -0700

    Define cars test inputs as fixtures. Assert outputs are within 5 decimal places of each other

--- tests/test_cars.py
+++ tests/test_cars.py
@@ -1,25 +1,44 @@
 from os import path
 
 from benchmarks.models.cars import cars_blackbox
-from numpy import array
-from numpy.testing import assert_array_equal
+from numpy import array, linspace, row_stack
+from numpy.testing import assert_array_almost_equal
 from pandas import read_csv
+from pytest import fixture
+from scipy.interpolate import interp1d
 
 TEST_DIR = path.dirname(__file__)
 
 
-def test_cars():
+@fixture
+def inputs():
+    inputs_path = path.join(TEST_DIR, "data", "cars_inputs.csv")
+    return read_csv(inputs_path)
+
+
+@fixture
+def times(inputs):
+    return inputs["t"].to_numpy().flatten()
+
+
+@fixture
+def signals(inputs):
+    signal_cols = inputs.columns[1:3]
+    return inputs[signal_cols].to_numpy().T
+
+
+@fixture
+def outputs():
     outputs_path = path.join(TEST_DIR, "data", "cars_outputs.csv")
-    outputs = read_csv(outputs_path)
+    return read_csv(outputs_path)
 
-    X = []
-    T = array([0, 20, 40, 60, 80, 100])
-    U = array([[0, 1, 0, 1, 0, 0], [1, 0, 1, 0, 1, 1]])
 
-    trajectories, timestamps = cars_blackbox._func(X, T, U)
+def test_cars(times, signals, outputs):
+    trajectories, timestamps = cars_blackbox._func([], times, signals)
     real_timestamps = outputs["t"].to_numpy()
-    real_trajectory_cols = ["x1", "x2", "x3", "x4", "x5"]
+    real_trajectory_cols = outputs.columns[1:6]
     real_trajectories = outputs[real_trajectory_cols].to_numpy()
 
-    assert_array_equal(real_timestamps, timestamps)
-    assert_array_equal(real_trajectories, trajectories)
+    assert_array_almost_equal(real_timestamps, timestamps, decimal=5)
+    assert_array_almost_equal(real_trajectories, trajectories, decimal=5)
+
