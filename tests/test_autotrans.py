c6e5e87940b5e5111105f265bc3eabe8436fb17d
Quinn Thibeault <qthibeau@asu.edu>
2021-04-08 14:57:18 -0700

    Create another autotrans test for state trajectory

--- tests/test_autotrans.py
+++ tests/test_autotrans.py
@@ -2,22 +2,29 @@ from os import path
 from unittest import TestCase
 
 from benchmarks.models.autotrans import _dosim
-from numpy import array
-from numpy.testing import assert_equal
+from numpy import array, float64
+from numpy.testing import assert_allclose, assert_equal
 from pandas import read_csv
 
 TEST_DIR = path.dirname(__file__)
 
 
 class AutotransTestCase(TestCase):
-    def test_correct_output(self):
-        inputs = read_csv(path.join(TEST_DIR,  "data", "autotrans_inputs.csv"))
-        outputs = read_csv(path.join(TEST_DIR, "data", "autotrans_outputs.csv"))
+    def setUp(self):
+        inputs_path = path.join(TEST_DIR,  "data", "autotrans_inputs.csv")
+        inputs = read_csv(inputs_path).to_numpy()
+        _, states, trajectories = _dosim(inputs[:, 0], inputs[:, 1:3].T)
 
-        T = inputs["t"].to_numpy()
-        U_cols = inputs.columns[1:3]
-        U = inputs[U_cols].to_numpy()
+        outputs_path = path.join(TEST_DIR, "data", "autotrans_outputs.csv")
+        self.outputs = read_csv(outputs_path).to_numpy()
+        self.states = states
+        self.trajectories = trajectories
 
-        _, _, trajectories = _dosim(T, U.T)
+    def test_trajectories(self):
+        real_trajectories = self.outputs[:, 0:2].astype(float64)
+        assert_allclose(self.trajectories, real_trajectories)
 
-        assert_equal(outputs.to_numpy(), trajectories)
+    def test_states(self):
+        real_states = self.outputs[:, 2]
+        assert_equal(self.states, real_states)
+
