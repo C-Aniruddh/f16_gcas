e80cb7fe367cd5cc58f8244376728637d31f6f89
Quinn Thibeault <qthibeau@asu.edu>
2021-04-08 15:16:18 -0700

    Convert from TestCase to pytest fixtures. Set output relative tolerance for test to 0.0002

--- tests/test_autotrans.py
+++ tests/test_autotrans.py
@@ -1,30 +1,31 @@
 from os import path
-from unittest import TestCase
 
 from benchmarks.models.autotrans import _dosim
 from numpy import array, float64
 from numpy.testing import assert_allclose, assert_equal
 from pandas import read_csv
+from pytest import fixture
 
 TEST_DIR = path.dirname(__file__)
 
 
-class AutotransTestCase(TestCase):
-    def setUp(self):
-        inputs_path = path.join(TEST_DIR,  "data", "autotrans_inputs.csv")
-        inputs = read_csv(inputs_path).to_numpy()
-        _, states, trajectories = _dosim(inputs[:, 0], inputs[:, 1:3].T)
+@fixture
+def inputs():
+    inputs_path = path.join(TEST_DIR,  "data", "autotrans_inputs.csv")
+    return read_csv(inputs_path).to_numpy()
 
-        outputs_path = path.join(TEST_DIR, "data", "autotrans_outputs.csv")
-        self.outputs = read_csv(outputs_path).to_numpy()
-        self.states = states
-        self.trajectories = trajectories
 
-    def test_trajectories(self):
-        real_trajectories = self.outputs[:, 0:2].astype(float64)
-        assert_allclose(self.trajectories, real_trajectories)
+@fixture
+def outputs():
+    outputs_path = path.join(TEST_DIR, "data", "autotrans_outputs.csv")
+    return read_csv(outputs_path).to_numpy()
 
-    def test_states(self):
-        real_states = self.outputs[:, 2]
-        assert_equal(self.states, real_states)
+
+def test_autotrans(inputs, outputs):
+    _, states, trajectories = _dosim(inputs[:, 0], inputs[:, 1:3].T)
+    real_trajectories = outputs[:, 0:2].astype(float64)
+    real_states = outputs[:, 2]
+
+    assert_allclose(trajectories, real_trajectories, rtol=2e-4)
+    assert_equal(states, real_states)
 
