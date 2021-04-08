17c6cceb9f9cbe6d800a6e3a1ea4121586397338
Quinn Thibeault <qthibeau@asu.edu>
2021-04-08 14:56:01 -0700

    Fix missing numpy dtypes. Have _dosim return raw data, and the blackboxes format it for staliro

--- benchmarks/models/autotrans.py
+++ benchmarks/models/autotrans.py
@@ -1,6 +1,6 @@
 from matlab import double as mdouble
 from matlab.engine import start_matlab
-from numpy import array, row_stack
+from numpy import array, float32, float64, row_stack
 from staliro.models import Blackbox
 
 MODEL_NAME = "Autotrans_shift"
@@ -14,26 +14,25 @@ def _dosim(T, U):
         eng = start_matlab()
 
     sim_t = mdouble([0, max(T)])
-    sim_inp = mdouble([row_stack((T, U)).T.tolist()])
+    sim_inp = mdouble(row_stack((T, U)).T.tolist())
     sim_opt = eng.simget(MODEL_NAME)
     sim_opt = eng.simset(sim_opt, "SaveFormat", "Array")
 
-    timestamps, states, data = eng.sim(MODEL_NAME, sim_t, sim_opt, sim_inp, nargout=3)
-    np_timestamps = array(timestamps, dtype=np.float32).flatten()
-    np_states = array(states, dtype=int)
-    np_data = array(data, dtype=np.float64).T
+    timestamps, _, data = eng.sim(MODEL_NAME, sim_t, sim_opt, sim_inp, nargout=3)
+    np_timestamps = array(timestamps, dtype=float32).flatten()
+    np_data = array(data, dtype=float64)
 
-    return np_timestamps, np_states, np_data
+    return np_timestamps, np_data[:, 2], np_data[:, 0:2]
 
 
 @Blackbox
 def autotrans_blackbox(_, T, U):
     timestamps, _, trajectories = _dosim(T, U)
-    return trajectories, timestamps
+    return trajectories.T, timestamps
 
 
 @Blackbox
 def autotrans_gears_blackbox(_, T, U):
     timestamps, states, _ = _dosim(T, U)
-    return states, timestamps
+    return array([states]), timestamps
 
