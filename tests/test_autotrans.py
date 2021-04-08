f4de3c46ba880e0a814beb9acb30cafd275b857e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-04-08 11:20:29 -0700

    update test for autotrans model

--- tests/test_autotrans.py
+++ tests/test_autotrans.py
@@ -1,21 +1,21 @@
 from os import path
 from unittest import TestCase
 
-from benchmarks.models.autotrans import sim_autotrans
+from benchmarks.models.autotrans import _dosim
 from numpy import array
 from numpy.testing import assert_equal
 from pandas import read_csv
 
+TEST_DIR = path.dirname(__file__)
+
 
 class AutotransTestCase(TestCase):
     def test_correct_output(self):
-        test_dir = path.dirname(__file__)
-        inputs = read_csv(path.join(test_dir, "test_autotrans_inputs.csv"))
-        outputs = read_csv(path.join(test_dir, "test_autotrans_outputs.csv"))
+        inputs = read_csv(path.join(TEST_DIR, "test_autotrans_inputs.csv"))
+        outputs = read_csv(path.join(TEST_DIR, "test_autotrans_outputs.csv"))
 
-        simT = 30
         T = inputs["t"].tonumpy()
         U = inputs[["u1", "u2"]].tonumpy()
-        trajectories, _ = sim_autotrans(simT, T, U)
+        _, _, trajectories = _dosim(T, U)
 
         assert_equal(outputs.tonumpy(), trajectories)
