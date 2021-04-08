4c2f7b5458ca2e441262b04263419948916e149c
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-04-08 11:12:28 -0700

    Create new autotrans blackbox that returns system states Abstract simulation logic into common method

--- benchmarks/models/autotrans.py
+++ benchmarks/models/autotrans.py
@@ -1,25 +1,38 @@
 import matlab
 import matlab.engine
 import numpy as np
+from staliro.models import Blackbox
 
-engs = {}
 MODEL_NAME = "Autotrans_shift"
+eng = None
 
 
-def sim_autotrans(simT, T, u, tag="default"):
-    global engs
+def _dosim(T, U):
+    global eng
 
-    if tag not in engs:
-        engs[tag] = matlab.engine.start_matlab()
+    if eng is None:
+        eng = matlab.engine.start_matlab()
 
-    eng = engs[tag]
-    simT = matlab.double([0, float(simT)])
-    simInp = matlab.double(np.row_stack((T, u)).T.tolist())
-    simOpt = eng.simget(MODEL_NAME)
-    simOpt = eng.simset(simOpt, "SaveFormat", "Array")
+    sim_t = matlab.double([0, max(T)])
+    sim_inp = matlab.double([np.row_stack(T, U).T.tolist()])
+    sim_opt = eng.simget(MODEL_NAME)
+    sim_opt = eng.simset(sim_opt, "SaveFormat", "Array")
 
-    timestamps, _, data = eng.sim(MODEL_NAME, simT, simOpt, simInp, nargout=3)
+    timestamps, states, data = eng.sim(MODEL_NAME, sim_t, sim_opt, sim_inp, nargout=3)
     np_timestamps = np.array(timestamps, dtype=np.float32).flatten()
+    np_states = np.array(states, dtype=int)
     np_data = np.array(data, dtype=np.float64).T
 
-    return np_data, np_timestamps
+    return np_timestamps, np_states, np_data
+
+
+@Blackbox
+def autotrans_blackbox(_, T, U):
+    timestamps, _, trajectories = _dosim(T, U)
+    return timestamps, trajectories
+
+
+@Blackbox
+def autotrans_gears_blackbox(_, T, U):
+    timestamps, states, _ = _dosim(T, U)
+    return timestamps, states
