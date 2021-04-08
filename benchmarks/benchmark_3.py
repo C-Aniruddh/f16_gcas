03c92e3f2b9634ed5fc8e8a2a98c47b24af8fc5f
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-04-08 11:23:37 -0700

    Update benchmark 3 options

--- benchmarks/benchmark_3.py
+++ benchmarks/benchmark_3.py
@@ -1,6 +1,6 @@
 from numpy import array
 from staliro import staliro
-from staliro.options import Options
+from staliro.options import Options, SignalOptions
 from staliro.optimizers import partitioning
 from staliro.optimizers.partitioning import PartitioningOptions, SamplingMethod
 from tltk_mtl import Predicate
@@ -15,8 +15,34 @@ class Benchmark3(Benchmark):
         self.preds = {
             "gear1": Predicate(array([[1, 0, 0], [-1, 0, 0]]), array([0.5, -1.5]))
         }
-        self.options = Options()
-        self.optimizer_options = PartitioningOptions()
+        self.options = Options(
+            runs=50,
+            iterations=100,
+            seed=131013014,
+            interval=(0, 30),
+            static_parameters=[],
+            signals=[
+                SignalOptions((0, 100), control_points=7),
+                SignalOptions((0, 350), control_points=3),
+            ],
+        )
+        dimensions = len(self.options.bounds)
+        self.optimizer_options = PartitioningOptions(
+            subregion_file="/tmp/subregions_benchmark3.csv",
+            region_dimension=dimensions,
+            num_partition=2,
+            miscoverage_level=0.05,
+            num_sampling=20,
+            level=[0.5, 0.75, 0.9, 0.95],
+            min_volume=0.001,
+            max_budget=15_000,
+            fal_num=50_000,
+            n_model=20,
+            n_bo=10,
+            n_b=100,
+            sample_method=SamplingMethod.BAYESIAN,
+            part_num=1,
+        )
 
     def run(self):
         return staliro(
